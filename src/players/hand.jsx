37f9d9cd12620e4c1e3e50cb175c4e2637ce51a8
FloezeTv <mathematikschlumpf@gmail.com>
2022-02-05 00:03:11 +0100

    Fixed indexing of cards to get correct animations

--- src/players/hand.jsx
+++ src/players/hand.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import Card from '../components/card';
 import style from './hand.module.css';
 
+const getDistinctPostfix = (array, index, equals = (a, b) => Object.keys(a).every(k => a[k] === b[k])) => {
+    const compare = array[index];
+    let postfix = -1;
+    for(let i = index - 1; i >= 0; i--)
+        if(equals(compare, array[i]))
+            postfix++;
+    return postfix >= 0 ? postfix.toString() : "";
+}
+
 class Hand extends React.Component {
 
     render() {
@@ -10,7 +19,7 @@ class Hand extends React.Component {
 
         return (
             <div className={style.hand}>
-                {this.props.cards.map((card, index) => (<Card color={card.color} value={card.value} key={index} onClick={() => this.props.onClick && this.props.onClick(card)} />))}
+                {this.props.cards.map((card, index) => (<Card color={card.color} value={card.value} key={card.color + "-" + card.value + getDistinctPostfix(this.props.cards, index)} onClick={() => this.props.onClick && this.props.onClick(card)} />))}
             </div>
         );
     }
@@ -23,7 +32,7 @@ const colorOrder = ["red", "yellow", "green", "blue", "black"];
 const valueOrder = [...Array(10).keys(), "reverse", "skip", "draw", "wild", "wild+4"];
 const sortingFunction = (a, b) => {
     const colorDiff = colorOrder.indexOf(a.color) - colorOrder.indexOf(b.color);
-    if (colorDiff != 0)
+    if (colorDiff !== 0)
         return colorDiff;
     return valueOrder.indexOf(a.value) - valueOrder.indexOf(b.value);
 };
